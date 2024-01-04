cec4daf67572f23912a11c0e724ee122a6a1b7de
GuoYuanxinKevin <118010084@link.cuhk.edu.cn>
2024-01-04 10:43:35 -0500

    Update E_BB_0_graph.m

--- E_BB_0_graph.m
+++ E_BB_0_graph.m
@@ -4,9 +4,9 @@
 % Setting up constants
 % Compute E_BB(p1, a, 0, a ★ p0)
 
-p0 = 0.05;
-p1 = 0.2;
-R = 0.6;
+p0 = 0.01;
+p1 = 0.45;
+R = 0.9;
 a = inv_h(1-R);
 
 p = p1;
@@ -17,7 +17,7 @@ step = a / 20;
 rr = 0:step:a;
 E = [];
 for r = 0:step:a
-    E = [E -h_b(a) + h_b(r) + E_BT_0(p,r,t)];
+    E = [E h_b(a) - h_b(r) + E_BT_0(p,r,t)];
 end
 
 % It can be seen from the graph that [-h_b(a) + h_b(r) + E_BT(p,r,0,t)]
